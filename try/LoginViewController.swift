df29cda3f40089bceaad998e68e9dc018b1798b6
junyiz <junyiz@umich.edu>
2020-04-20 14:19:58 -0400

    final version?

--- try/LoginViewController.swift
+++ try/LoginViewController.swift
@@ -12,26 +12,61 @@ import FirebaseAuth
 import GoogleSignIn
 
 class LoginViewController: UIViewController, GIDSignInDelegate {
-    @IBOutlet weak var email: UITextField!
-    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var btn_sign_out: UIButton!
+    @IBOutlet weak var signinBtn: GIDSignInButton!
+    @IBOutlet weak var confirmBtn: UIButton!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     
+    @IBAction func btnSignOutAction(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            self.btn_sign_out.isHidden = true
+            self.confirmBtn.isHidden = true
+            self.signinBtn.isHidden = false
+            self.welcomeLabel.isHidden = true
+            nameLabel.text = "Please login"
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
     
     // constants
     let userDefault = UserDefaults.standard
-    
+    func currentUserName() {
+        if let currentUser = Auth.auth().currentUser {
+            self.btn_sign_out.isHidden = false
+            self.confirmBtn.isHidden = false
+            self.signinBtn.isHidden = true
+            self.welcomeLabel.isHidden = false
+            nameLabel.text = currentUser.displayName ?? "DISPLAY NAME NOT FOUND"
+        }
+    }
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if error != nil {
-            email.text = "leongong@umich.edu"
             print(error.localizedDescription)
             return
-        } else {
-            email.text = "leongong@umich.edu"
+        }
+        guard let authentication = user.authentication else {return}
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if (error != nil) {
+                return
+            } else {
+                self.currentUserName()
+            }
         }
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.btn_sign_out.isHidden = true
+        self.confirmBtn.isHidden = true
+        self.signinBtn.isHidden = false
+        self.welcomeLabel.isHidden = true
+        nameLabel.text = "Please login"
         GIDSignIn.sharedInstance()?.delegate = self
         GIDSignIn.sharedInstance()?.presentingViewController = self
         // Do any additional setup after loading the view.
